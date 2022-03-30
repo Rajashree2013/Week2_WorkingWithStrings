1e7b88093a6ee4a96237c52881642aa29eaefbdf
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 11:03:23 +0200

    String Length check

---
+++ src/main/java/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String txt = "java";
+        System.out.println("The length of the txt string is: " + txt.length());
+    }
+}
