cb669042439fa132e9d15002467aa43d2365f4f3
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 17:17:13 +0200

    Convert TO LowerCase To Upercase

---
+++ src/main/java/ConvertToLowerCaseToUperCase.java
@@ -0,0 +1,16 @@
+public class ConvertToLowerCaseToUperCase {
+
+
+    public static void main(String[] args) {
+
+        String str1 = "CAPS EQUALS SCREAMING";
+        str1 = str1.toLowerCase();
+        System.out.println(str1);
+
+        String str2 = str1.toUpperCase();
+        System.out.println(str2);
+
+
+
+    }
+}
