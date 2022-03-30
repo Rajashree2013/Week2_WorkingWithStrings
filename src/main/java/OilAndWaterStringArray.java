05bd551c971ae596b0bb11c2b564996afab72927
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 17:25:34 +0200

    Oil and Water String Arrey

---
+++ src/main/java/OilAndWaterStringArray.java
@@ -0,0 +1,23 @@
+public class OilAndWaterStringArray {
+
+    public static void main(String[] args) {
+
+        String strData = "Oil and Water";
+        String strOil = strData.substring(0,3);
+        String strWater = strData.substring(8);
+
+        //System.out.println(str_Oil);
+        //System.out.println(str_Water);
+
+
+
+        String[] strOilWater = new String[2];
+        strOilWater[0] = strOil;
+        strOilWater[0] = strWater;
+
+
+
+
+    }
+
+}
