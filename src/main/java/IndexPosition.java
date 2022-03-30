42c3e72cd61f942d42db01493a0afb61576e5e2b
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 11:32:06 +0200

    Char position index

---
+++ src/main/java/IndexPosition.java
@@ -0,0 +1,13 @@
+public class IndexPosition {
+
+        public static <Char> void main(String[] args) {
+            String txt = "Long example sentence";
+            System.out.println(txt.charAt(6));
+        }
+    }
+
+
+
+
+
+
