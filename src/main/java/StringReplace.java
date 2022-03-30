3ca0dad4d7d9c912b2e5a831086336ccdb0a083f
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 17:44:36 +0200

    String replace word

---
+++ src/main/java/StringReplace.java
@@ -0,0 +1,8 @@
+public class StringReplace {
+    public static void main(String[] args) {
+        String strWord = "Java is the worst programming language!" ;
+        String replaceString=strWord.replace("worst","best");
+        System.out.println(replaceString);
+
+    }
+}
