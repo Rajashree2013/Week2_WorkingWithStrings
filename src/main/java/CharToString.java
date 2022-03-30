d2ab9c5b409b576659fdaad7d6f93e7eb7dcc5f3
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 17:14:23 +0200

    Char To String

---
+++ src/main/java/CharToString.java
@@ -0,0 +1,10 @@
+public class CharToString {
+
+    public static void main(String[] args) {
+
+        char[] arr = { 'J', 'A', 'V', 'A' };
+        String str = String.valueOf(arr);
+        System.out.println(str);
+
+    }
+}
