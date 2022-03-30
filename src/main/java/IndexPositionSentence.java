4ccbd599f87d4352a5cab1235af4844d28fb9b0c
Rajashree <bhuyan.rajashree@gmail.com>
2022-03-30 17:21:03 +0200

    IndexPositionSentence

---
+++ src/main/java/IndexPositionSentence.java
@@ -0,0 +1,12 @@
+public class IndexPositionSentence {
+
+    public static void main(String[] args) {
+        String se = "Even longer example sentence!";
+        System.out.println(se.indexOf("o"));
+    }
+}
+
+
+
+
+
